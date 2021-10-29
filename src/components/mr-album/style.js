91345d78288f5b77e33571be02c13adbf0b96850
liulin5987 <1152266212@qq.com>
2021-10-29 11:33:36 +0800

    'album'

---
+++ src/components/mr-album/style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const AlbumWrapper = styled.div`
+display: flex;
+flex-direction: column;
+width: ${props => props.width + 'px'};
+
+/* height: 150px; */
+.album-top {
+  width: ${props => props.width + 'px'};
+  height: ${props => props.size + 'px'};
+  position: relative;
+  margin-top: 15px;
+  .album-cover {
+    width: ${props => props.size + 'px'};
+    height: ${props => props.size + 'px'};
+  }
+  .album-cloak {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-position: 0 ${props => props.bgp};
+  }
+}
+.album-bottom {
+  font-size: 12px;
+    width: ${props => props.size};
+    .name {
+      color: #000;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+
+    .artist {
+      color: #666;
+    }
+}
+`
--- src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
@@ -1,27 +1,58 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import MRRecHeader from '@/components/rec-header'
-import { useDispatch,useSelector } from 'react-redux'
 import { getAlubmAction } from '../../store/actionCreaters'
+import { Carousel } from 'antd'
+import MRAlbum from '../../../../../../components/mr-album'
+import { AlbumsWrapper } from './style'
 
 export default memo(function MRNewAlbum() {
   // state
   // redux hooks
   const dispatch = useDispatch()
-  const {albums} = useSelector(state => ({
+  const { albums } = useSelector((state) => ({
     albums: state.getIn(['recommend', 'albums'])
   }))
   console.log('专辑信息', albums)
   // other hooks
+  const pageRef = useRef()
   useEffect(() => {
     dispatch(getAlubmAction())
-    return () => {
-      
-    }
+    return () => {}
   }, [dispatch])
   return (
-    <div>
-      <MRRecHeader title="新碟上架"></MRRecHeader>
-    </div>
+    <AlbumsWrapper>
+      <MRRecHeader title='新碟上架'></MRRecHeader>
+      <div className='content'>
+        <button
+          className='arrow arrow-left sprite_02'
+          onClick={(e) => pageRef.current.prev()}
+        ></button>
+        <div className='album-wrapper'>
+          <Carousel dots={false} ref={pageRef}>
+            {[0, 5].map((item) => {
+              return (
+                <div className='albums'>
+                  {albums.slice(item, item + 5).map((aItem) => {
+                    return (
+                      <MRAlbum
+                        info={aItem}
+                        key={aItem.id}
+                        size={100}
+                        width={118}
+                        bgp='-570px'
+                      ></MRAlbum>
+                    )
+                  })}
+                </div>
+              )
+            })}
+          </Carousel>
+        </div>
+        <button className="arrow arrow-right sprite_02"
+                onClick={e => pageRef.current.next()}></button>
+      </div>
+    </AlbumsWrapper>
   )
 })
